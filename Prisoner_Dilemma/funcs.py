8c6b797ec828999c1e2144262ed00ccff6c40cb0
Ivan Jordaan <jordaanivan@gmail.com>
2022-07-11 21:50:32 -0400

    Various Changes

--- Prisoner_Dilemma/funcs.py
+++ Prisoner_Dilemma/funcs.py
@@ -1,38 +1,42 @@
 import random
 
-def investigate(seq,num,prisoner,count=0):
-    if seq[num] != prisoner:
-        count += 1
-        if count > 0.5*len(seq):
-            return [0, -1]
+def investigate(seq,num,prisoner,guesses,count=0):
+    ticket = seq[num]
+    if ticket != prisoner:
         num = seq[num]
-        return investigate(seq,num,prisoner,count)
+        count += 1
+        if count > guesses:
+            return 0, -1
+        else:
+            return investigate(seq,num,prisoner,count)
     else:
-        return [1,count]
+        return 1, count
 
 def initialize(n):
     seq = random.sample(range(n),n)
     unique_prisoners = list(range(n))
     return seq, unique_prisoners
 
-def one_run(num_of_prisoners):
+def one_run(num_of_prisoners,guesses):
     seq, unique_prisoners = initialize(num_of_prisoners)
     res = []
     counts = []
     while unique_prisoners:
         num = random.choice(unique_prisoners)
-        res.append(investigate(seq,num,num)[0])
-        counts.append(investigate(seq,num,num)[1])
+        result, count = investigate(seq,num,num,guesses)
+        res.append(result)
+        counts.append(count)
         unique_prisoners.remove(num)
     if sum(res)/len(res) == 1:
-        return [1,counts]
+        return 1, counts
     else:
-        return [0,counts]
+        return 0, counts
 
-def many_runs(number_of_runs,number_of_prisoners):
+def many_runs(number_of_runs,number_of_prisoners,guesses):
     res = []
     counts = []
     for i in range(number_of_runs):
-        res.append(one_run(number_of_prisoners)[0])
-        counts.append(one_run(number_of_prisoners)[1])
+        result, count = one_run(number_of_prisoners,guesses)
+        res.append(result)
+        counts.append(count)
     return res, counts
